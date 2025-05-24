a6db558a00e8388bb4f847a13c734e158948a710
Rituraj Hao <ri2rajhao1@gmail.com>
2025-05-24 10:23:11 +0530

    little changes in theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,16 +5,6 @@ import Navbar from '@/Components/layout/Navbar/Navbar';
 import Footer from '@/Components/layout/Footer/Footer';
 import { Providers } from './Providers';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
 export const metadata = {
   title: 'Notes Cafe',
   description: 'Get your notes here',
@@ -23,21 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        document.documentElement.setAttribute(
-          'data-theme',
-          localStorage.getItem('theme') || 'light'
-        );
-      `,
-          }}
-        />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors duration-200`}
-      >
+      <head></head>
+      <body className={`antialiased transition-colors duration-200`}>
         <Providers>
           <Navbar />
           {children}
